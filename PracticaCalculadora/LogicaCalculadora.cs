c596a8feed28df455976ffba3e1d6d24b98c547b
FabianLeon95 <fabian30leon@gmail.com>
2018-10-20 10:48:33 -0600

    Se agrega la Clas LogicaCalculadora con los metodos del triangulo

---
+++ PracticaCalculadora/LogicaCalculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCalculadora
+{
+    class LogicaCalculadora
+    {
+        public double? PerimetroTriangulo(double a, double b, double c)
+        {
+            if (validarTriangulo(a, b, c))
+            {
+                double perimetro = a + b + c;
+                return perimetro;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public double? AreaTriangulo(double a, double b, double c)
+        {
+            if (validarTriangulo(a, b, c))
+            {
+                double s = (a + b + c) / 2;
+                double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+                return area;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool validarTriangulo(double a, double b, double c)
+        {
+            double mayor = a;
+            mayor = (b > mayor) ? b : mayor;
+            mayor = (c > mayor) ? c : mayor;
+            double sumaMenores = a + b + c - mayor;
+            if (sumaMenores > mayor)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
